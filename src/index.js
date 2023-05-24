e597690daf9c5b19e85cc5ee2b08eaf5719d8244
GabrielBTonelli <tonelli.gabriel@gmail.com>
2023-05-24 11:57:26 +0200

    State Management

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
 import Quiz from './components/Quiz';
+import { QuizProvider } from './contexts/quiz';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
   <React.StrictMode>
-    <Quiz />
+    <QuizProvider>
+      <Quiz />
+    </QuizProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
